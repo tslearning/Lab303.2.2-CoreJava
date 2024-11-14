96e84f45b783beac42e9619dc07d6d3f4a535e6b
Touyer Lee <touyer.lees@gmail.com>
2024-11-14 13:50:41 -0600

    Lab 303.2.2 - Core Java complete

--- src/ExplicitTest.java
+++ src/ExplicitTest.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class ExplicitTest {
+    public static void main(String[] args) {
+        double d = 100.04;
+
+        long l = (long)d;
+        int i = (int)l;
+        System.out.println("Double value " + d);
+        System.out.println("Long value " + l);
+        System.out.println("Int value " + 1);
+
+        byte b;
+        int z = 257;
+        double dou = 323.142;
+        System.out.println("Conversion of int to byte.");
+        b = (byte) z;
+        System.out.println(" = " + z + " b = " + b);
+        System.out.println("Conversion of double to int.");
+
+        z = (int) dou;
+        System.out.println("dou = " + dou + " b = " + z);
+        System.out.println("Conversion of double to byte.");
+
+        b = (byte) dou;
+        System.out.println("dou = " + dou + " b = " + b);
+
+    }
 }
